143049d7ff04be46e5b44a9e411c0fde93e47491
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-05-05 10:08:49 +0500

    test(main): adding first test

---
+++ tests/XmlTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vitkuz573\FreeId\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Vitkuz573\FreeId\Xml;
+
+final class XmlTest extends TestCase
+{
+    public function testXml()
+    {
+        $free_id = new Xml();
+        $this->assertTrue($free_id(__DIR__.'/test.xml', 'node') === 3);
+    }
+}
