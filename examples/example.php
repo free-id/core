5439bdf44fafc31b29f2d246f5c10382951920eb
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-05-09 10:05:20 +0500

    fix(main): update examples

--- examples/example.php
+++ examples/example.php
@@ -9,10 +9,13 @@ use Vitkuz573\FreeId\Parsers\Database\Sqlite;
 use Vitkuz573\FreeId\Parsers\File\Json;
 use Vitkuz573\FreeId\Parsers\File\Xml;
 
-const HOST = '127.0.0.1';
-const DATABASE = 'test';
-const TABLE = 'items';
-const CREDENTIALS = ['username' => 'root', 'password' => 'root'];
+const MYSQL_HOST = '127.0.0.1';
+const MYSQL_DATABASE = 'test';
+const MYSQL_TABLE = 'items';
+const MYSQL_CREDENTIALS = ['username' => 'root', 'password' => 'root'];
+
+const SQLITE_DATABASE = 'test.sqlite3';
+const SQLITE_TABLE = 'items';
 
 $xml = new Xml(__DIR__ . '/../tests/files/test.xml', 'node');
 echo 'Free ID (XML): ' . $xml->find() . PHP_EOL;
@@ -20,8 +23,8 @@ echo 'Free ID (XML): ' . $xml->find() . PHP_EOL;
 $json = new Json(__DIR__ . '/../tests/files/test.json', 'nodes');
 echo 'Free ID (JSON): ' . $json->find() . PHP_EOL;
 
-$mysql = new MySql(HOST, DATABASE, TABLE, CREDENTIALS);
+$mysql = new MySql(MYSQL_HOST, MYSQL_DATABASE, MYSQL_TABLE, MYSQL_CREDENTIALS);
 echo 'Free ID (MySQL): ' . $mysql->find() . PHP_EOL;
 
-$sqlite = new Sqlite('test.sqlite3', 'items', 'identifier');
+$sqlite = new Sqlite(SQLITE_DATABASE, SQLITE_TABLE);
 echo 'Free ID (SQLite): ' . $sqlite->find() . PHP_EOL;
