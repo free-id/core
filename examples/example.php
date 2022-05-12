3cd95f84fdd248f924b7d8796f9b5ec05e9499e0
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-05-12 13:04:17 +0500

    refactor(main): update example

--- examples/example.php
+++ examples/example.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-$start = microtime(true);
-
 include __DIR__ . '/../vendor/autoload.php';
 
 use Vitkuz573\FreeId\Parsers\Database\MySql;
@@ -29,19 +27,35 @@ const SQLITE_TABLE = 'items';
 
 echo PHP_EOL;
 
+$start_xml = microtime(true);
 $xml = new Xml(__DIR__ . '/../tests/files/test.xml', 'node');
 echo 'Free ID (XML): ' . $xml->find() . PHP_EOL;
+$stop_xml = (microtime(true) - $start_xml);
 
+$start_json = microtime(true);
 $json = new Json(__DIR__ . '/../tests/files/test.json', 'nodes');
 echo 'Free ID (JSON): ' . $json->find() . PHP_EOL;
+$stop_json = (microtime(true) - $start_json);
 
+$start_mysql = microtime(true);
 $mysql = new MySql(MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE, MYSQL_TABLE, MYSQL_CREDENTIALS);
 echo 'Free ID (MySQL): ' . $mysql->find() . PHP_EOL;
+$stop_mysql = (microtime(true) - $start_mysql);
 
+$start_pgsql = microtime(true);
 $pgsql = new PgSql(PGSQL_HOST, PGSQL_PORT, PGSQL_DATABASE, PGSQL_TABLE, PGSQL_CREDENTIALS);
 echo 'Free ID (PostgreSQL): ' . $pgsql->find() . PHP_EOL;
+$stop_pgsql = (microtime(true) - $start_pgsql);
 
+$start_sqlite = microtime(true);
 $sqlite = new Sqlite(SQLITE_DATABASE, SQLITE_TABLE);
 echo 'Free ID (SQLite): ' . $sqlite->find() . PHP_EOL;
+$stop_sqlite = (microtime(true) - $start_sqlite);
 
-echo PHP_EOL . 'The script was executed in ' . (microtime(true) - $start) . ' seconds';
+echo PHP_EOL . 'The XML parser was executed in ' . $stop_xml . ' seconds';
+echo PHP_EOL . 'The Json parser was executed in ' . $stop_json . ' seconds';
+echo PHP_EOL . 'The MySQL parser was executed in ' . $stop_mysql . ' seconds';
+echo PHP_EOL . 'The PostgreSQL parser was executed in ' . $stop_pgsql . ' seconds';
+echo PHP_EOL . 'The SQLite parser was executed in ' . $stop_sqlite . ' seconds';
+
+echo PHP_EOL;
